05df8f2fd038e635d76a46b27fcced5fa0ad84a0
Dean Pearce <dapearce@live.unc.edu>
2023-02-21 11:01:55 -0500

    standardize seed for dishonest_casino

--- dishonest_casino.m
+++ dishonest_casino.m
@@ -1,4 +1,5 @@
 % training data for the occasionally dishonest casino
+rng(224); % standardise seed
 % b1 = string of die rolls
 % h1 = sequence of hidden states. 1=fair, 2=loaded
 b1 = [6,4,3,4,3,1,2,1,2,2,3,1,6,6,3,3,4,6,4,2,6,4,3,5,1,2,6,6,6,1,3,4,6,1,1,2,6,6,6,5,4,2,4,5,2,1,2,2,3,4];
